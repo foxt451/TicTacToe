f20cf581bc5ba0c167b3c3ddd6104f6b8b0e749e
Sviatozar <svpetrenko123@gmail.com>
2021-05-07 20:28:56 +0300

    Implement camera drag

---
+++ Assets/Scripts/Camera/MousePanner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePanner : MonoBehaviour
+{
+    public float dragSpeed = 2f;
+    private Vector3 originMousePos;
+    private Vector3 originCameraWorldPos;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // clicking the button
+        if (Input.GetMouseButtonDown(1))
+        {
+            originMousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            originCameraWorldPos = transform.position;
+        }
+        // has the button held
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 curPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 deltaPos = curPos - originMousePos;
+            transform.position = originCameraWorldPos - deltaPos * dragSpeed;
+        }
+    }
+}
